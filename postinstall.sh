315581adddbdb4bbf5ccd63d3dcb777ef0eb79b2
Marni <marnix@roboco.dev>
2023-07-30 18:03:30 +0200

    Fix postinstall

--- postinstall.sh
+++ postinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget https://media.discordapp.net/attachments/977197179477327903/1052649070587551824/Honeycam_.gif -O frames/source.gif
 # Extract all frames from the source gif
-convert frames/source.gif src/frames/frame%02d.png
+convert frames/source.gif frames/frame%02d.png
 # Clean up the source gif
 rm frames/source.gif
